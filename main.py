5d253ddc753ff69281a296f28faf097976744ccb
LogicalAlmond <logicalalmond@protonmail.com>
2023-01-25 20:18:51 -0500

    testing

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ result, model_outputs, wrong_predictions = model.eval_model(eval_df)
 # Make predictions with the model
 predictions, raw_outputs = model.predict(["Sam was a king"])
 
-print(predictions)
+print(result)
